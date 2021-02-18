503da77f4b495a9626e46692334c51d44a9debd4
Jessica Yee <jessicayee@Jessicas-MacBook-Pro.local>
2021-02-17 21:54:37 -0500

    add buttons to tables

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,8 +6,7 @@ function Home() {
       <h1>College Prep Resource Hub</h1>
       <h3>(For first-generation, low-income high school students)</h3>
       <div className="important-dates-sect">
-        <h2>HP: Insert Important Dates Here</h2>
-
+        <h2>Financial Aid Deadlines</h2>
         <table>
           <tbody>
             <tr>
@@ -20,7 +19,14 @@ function Home() {
             </tr>
             <tr>
               <td>2021-22 FAFSA State Deadline</td>
-              <td>Varies by state. SEE HERE.</td>
+              <td>
+                <a
+                  target="_blank"
+                  href="https://studentaid.gov/apply-for-aid/fafsa/fafsa-deadlines"
+                >
+                  Varies by state - info here
+                </a>
+              </td>
             </tr>
             <tr>
               <td>2021 CSS Profile</td>
@@ -29,6 +35,7 @@ function Home() {
           </tbody>
         </table>
 
+        <h2>Virtual College Fairs</h2>
         <table>
           <tbody>
             <tr>
@@ -39,30 +46,76 @@ function Home() {
             <tr>
               <td>General College Fair</td>
               <td>Feb. 28 @ 1:00pm - 7:00pm EDT</td>
-              <td>Register Here</td>
+              <td>
+                <button>
+                  <a
+                    target="_blank"
+                    href="https://virtualcollegefairs.org/events/42/exhibitors"
+                  >
+                    Register Here
+                  </a>
+                </button>
+              </td>
             </tr>
+
             <tr>
               <td>STEM College Fair</td>
               <td>March 7 @ 1:00pm - 5:00pm EDT</td>
-              <td>Register Here</td>
+              <td>
+                <button>
+                  <a
+                    target="_blank"
+                    href="https://virtualcollegefairs.org/events/80/exhibitors"
+                  >
+                    Register Here
+                  </a>
+                </button>
+              </td>
             </tr>
 
             <tr>
               <td>Performing & Visual Arts College Fair</td>
               <td>March 16 @ 4:00pm - 8:00pm EDT</td>
-              <td>Register Here</td>
+              <td>
+                <button>
+                  <a
+                    target="_blank"
+                    href="https://virtualcollegefairs.org/events/83/exhibitors"
+                  >
+                    Register Here
+                  </a>
+                </button>
+              </td>
             </tr>
 
             <tr>
               <td>Western U.S. College Fair</td>
-              <td>April 10 @ 12:00pm - 4:00pm PDT</td>
-              <td>Register Here</td>
+              <td>April 10 @ 3:00pm - 7:00pm EDT</td>
+              <td>
+                <button>
+                  <a
+                    target="_blank"
+                    href="https://virtualcollegefairs.org/events/84/exhibitors"
+                  >
+                    Register Here
+                  </a>
+                </button>
+              </td>
             </tr>
 
             <tr>
               <td>Southeastern U.S. College Fair</td>
               <td>April 20 @ 3:00pm - 7:00pm EDT</td>
-              <td>Register Here</td>
+              <td>
+                <button>
+                  <a
+                    target="_blank"
+                    href="https://virtualcollegefairs.org/events/85/exhibitors"
+                  >
+                    Register Here
+                  </a>
+                </button>
+              </td>
             </tr>
           </tbody>
         </table>
